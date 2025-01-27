c0e585273eff82498af887d6590f2b60ce3f2e1d
Ibrahim Alsuwayyigh <Ibrahimalsuwayyigh@gmail.com>
2025-01-27 05:32:34 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Fingerprint Based ATM 2"
+include(":app")
